52e94f1f2614845a8208205c2a85b7bbabffee3a
Sahil Rai <78111534+Rai-Sahil@users.noreply.github.com>
2022-11-06 01:14:53 -0800

    Ask JeeHo

--- dynamicStruct.c
+++ dynamicStruct.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 
-//We have o define malloc to the data character so that's why we use pointer sign in front of data char
+///We have to define malloc to the data character so that's why we use pointer sign in front of data char
+///Whenever we try to give malloc to a variable, we need that variable to be pointer.
 typedef struct Student
 {
     char *data;
@@ -10,14 +10,18 @@ typedef struct Student
 }Student;
 
 int main(int argc, char **argv){
+    //TODO ask jeeho how to allocate memory for this Student array.
     Student* student = (Student*) malloc(sizeof(Student));
     char *buffer = (char*) malloc(sizeof(char*));
 
     FILE* file = fopen(argv[1], "r");
 
+    //TODO Also ask jee ho what to put an the place of 200.
     fgets(buffer, 200, file);
     int i = 0;
+
     while(!feof(file)){
+        ///Jump to the next pointer address.
         Student *s = student + i;
         student[i].data = (char*) malloc(sizeof(char*));
         sscanf(buffer, "%s %d", s->data, &s->d2);
