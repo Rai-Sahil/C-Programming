9191d97423d1d8869d5f8988f1bf53b916663f14
Sahil Rai <78111534+Rai-Sahil@users.noreply.github.com>
2022-11-07 12:44:48 -0800

    ---------Minor Bugs Fixed---------

--- dynamicStruct.c
+++ dynamicStruct.c
@@ -11,30 +11,23 @@ typedef struct Student
 
 int main(int argc, char **argv){
     ///Now instantiate an struct from variable student.
-    //TODO ask jeeho how to allocate memory for this Student array.
-    Student* student = (Student*) malloc(sizeof(Student));
-    
+    //TODO
+    Student student[1000];
+
     ///To cpature line by line from file.
     char *buffer = (char*) malloc(sizeof(char*));
 
     FILE* file = fopen(argv[1], "r");
 
-    //TODO Also ask jee ho what to put an the place of 200.
-    fgets(buffer, 200, file);
     int i = 0;
 
     while(!feof(file)){
-        //Giving memory address to student linked list
         Student *s = student + i;
-        //Hence both the data'ss are char, malloc should also be char.
         student[i].data = (char*) malloc(sizeof(char*));
-        sscanf(buffer, "%s %d", s->data, &s->d2);
-        fgets(buffer, 200, file);
+        fscanf(file, "%s %d", s->data, &s->d2);
         i++;
     }
-
-
-
+    
     for (int j = 0; j < i; ++j) {
         printf("%s %d\n", student[j].data, student[j].d2);
     }
